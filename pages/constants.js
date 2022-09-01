fc44e074eb7835ec25ab31d59d2df0a656e18b97
anemharikrishna@gmail.com <anemharikrishna@gmail.com>
2022-09-01 10:28:50 +0530

    fix

---
+++ pages/constants.js
@@ -0,0 +1,26 @@
+import { toast } from 'react-toastify';
+
+export const BASE_URL = 'https://nodejs-signup-signin-apis.up.railway.app/api/v1';
+
+export const toastError = (err) => {
+    toast.error(err, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+}
+export const toastSuccess = (msg) => {
+  toast.success(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+}
