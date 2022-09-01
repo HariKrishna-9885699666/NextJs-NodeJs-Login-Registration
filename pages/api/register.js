ac2502b6ef10eca037e438c39f458c6f8b2849d7
anemharikrishna@gmail.com <anemharikrishna@gmail.com>
2022-09-01 09:57:53 +0530

    fix

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,7 +1,7 @@
-import { BASE_URL } from '../../pages/constants';
+// import { BASE_URL } from '../../pages/constants';
 
 export default async function registerAPI (data) {
-    const response = await fetch(BASE_URL + '/register', {
+    const response = await fetch('https://nodejs-signup-signin-apis.up.railway.app/api/v1' + '/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- pages/constants.js
+++ pages/constants.js
@@ -1,33 +1,33 @@
-import { toast } from 'react-toastify';
+// import { toast } from 'react-toastify';
 
-const BASE_URL = 'https://nodejs-signup-signin-apis.up.railway.app/api/v1';
+// const BASE_URL = 'https://nodejs-signup-signin-apis.up.railway.app/api/v1';
 
-const toastError = (err) => {
-    toast.error(err, {
-      position: "top-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
-}
-const toastSuccess = (msg) => {
-  toast.success(msg, {
-    position: "top-right",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-  });
-}
+// const toastError = (err) => {
+//     toast.error(err, {
+//       position: "top-right",
+//       autoClose: 5000,
+//       hideProgressBar: false,
+//       closeOnClick: true,
+//       pauseOnHover: true,
+//       draggable: true,
+//       progress: undefined,
+//     });
+// }
+// const toastSuccess = (msg) => {
+//   toast.success(msg, {
+//     position: "top-right",
+//     autoClose: 5000,
+//     hideProgressBar: false,
+//     closeOnClick: true,
+//     pauseOnHover: true,
+//     draggable: true,
+//     progress: undefined,
+//   });
+// }
 
 
-export default {
-    BASE_URL,
-    toastError,
-    toastSuccess
-}
+// export {
+//     BASE_URL,
+//     toastError,
+//     toastSuccess
+// }
