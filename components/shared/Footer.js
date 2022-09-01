dcd747586234e551e7f71b6fc869a5841612ff04
Hari Krishna Anem <anemharikrishna@gmail.com>
2022-09-01 19:10:25 +0530

    Update Footer.js

--- components/shared/Footer.js
+++ components/shared/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () =>
     <footer className="footer mt-auto py-3 bg-light">
         <div className="container text-center">
-            <span className="text-muted">All Rights Reverved</span>
+            <span className="text-muted">All Rights Reserved</span>
         </div>
     </footer>
 
